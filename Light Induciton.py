83f55f51293430acee66c7369e98f72350faa817
Cecile <cecile.geier@gmail.com>
2017-07-26 17:13:49 +0100

    26/08

--- Light Induciton.py
+++ Light Induciton.py
@@ -13,7 +13,7 @@ def light(k,L,n,K1):
     a=444.6
 
     # k:  Maximum expression due to induction
-    # K1: Hill constant
+    # K1: Hill constant (W/m^2)
     # n: Hill coefficient
     # a: basal expression level of the promoter
     # L: light intensity (W/m^2)
@@ -26,49 +26,33 @@ def light(k,L,n,K1):
 
 print k1_rate_array
 
-EI=200 #initial concentration of EL222
+EI=0.25 #initial concentration of EL222 (T.F./cell)
 
 def diff_eqs(y, t):
     '''This function contains the differential equations'''
 
     """Unpacking y"""
-    EI = y[0]
-    EHB= y[1] #bound EL222 to the promoter
-    mRNA= y[2] #transcrption
-    P= y[3] #translation
-    PA= y[4] #expression on surface
+
+    EHB= y[0] #bound EL222 to the promoter
+    mRNA= y[1] #transcrption
+    P= y[2] #translation
+    PA= y[3] #expression on surface
 
     """Set rate constants""" #we made these numbers up we are now looking into fixing them and adding rate equations for the k values
 
-    k2= (660/360) #rate of transcription (nucleotides/min) 2,802 nucleotides
-    k3= 0.001 #rate of translation (/min)
-    d1= 1/5
-    d2= 1/5 #Degradation of transcript (mRNA)(/min)
-    d3= 1/20 #Degradation of protein (Half-life of E.coli) (/min)
-    b= 0.03 #Rate of transport of the protein to the surface of the membrane
+    k2= 60 #rate of transcription (nucleotides/s)
+    k3= 0.00006 #rate of translation (/s)
+    d1= 0.00333
+    d2= 0.00333 #Degradation of transcript (mRNA)(/s)
+    d3= 0.00083 #Degradation of protein (Half-life of E.coli) (/s)
+    b= 0.0005 #Rate of transport of the protein to the surface of the membrane (/s)
 
-    dEI_dt =-(light_intensity * EI )
 
     # Rate of EL222 being activated by light and binding to the promoter
-    dEHB_dt = (light_intensity * EI )-(k2*mRNA)-(d1*EHB)
-
-
-
-    #return EHB (After making an array)
-
-    #input Hill equation as a function, where EHB feeds into it
-    #According to the EHB concentration a different constant will be produced
-    #That constant will then be introduced as a factor for the transcription step
-
-    #We want to make a function that contains time as a dependant variable
-    #Where we want to say that the concentration of EHB will affect the time at which
-    #transcrption starts taking place, but not the rate at which transcription takes
-    #place
-
-
+    dEHB_dt = (light_intensity * EI )-(k2* EHB)-(d1*EHB)
 
     # Rate of transcription
-    dmRNA_dt = (k2*mRNA - (d2*mRNA)-(k3*mRNA))
+    dmRNA_dt = (k2*EHB) - (d2*mRNA)-(k3*mRNA)
 
     # Rate of translation
     dP_dt = (k3 * mRNA)-(d3*P)-(b*P)
@@ -77,23 +61,23 @@ def diff_eqs(y, t):
     dPA_dt= b*P
 
     """Repack solution in same order as y"""
-    sol = [dEI_dt, dEHB_dt, dmRNA_dt, dP_dt, dPA_dt]
+    sol = [dEHB_dt, dmRNA_dt, dP_dt, dPA_dt]
 
     return sol
 
 if __name__ == "__main__":
     time_steps = 1000 # Number of timepoints to simulate
-    t = np.linspace(0, 1200, time_steps)  # Set the timeframe (start_time, stop_time, step)
+    t = np.linspace(0, 12000, time_steps)  # Set the timeframe (start_time, stop_time, step)
 
     '''Set initial species concentration values'''
-    EI_0=0.00001          # Starting EL222 concentration in its inactive conformation in the cell
+    EI_0=0.25          # Starting EL222 concentration in its inactive conformation in the cell
     EHB_0=0           # Starting concentration of EL222 bound to the promoter
     mRNA_0=0            # Starting mRNA concentration
     P_0 = 0             # Starting protein concentration
     PA_0=0              #Starting concentration of protein expressed on the surface of the
 
     '''Pack intial conditions into an array'''
-    y0 = [EI_0, EHB_0, mRNA_0, P_0, PA_0]
+    y0 = [EHB_0, mRNA_0, P_0, PA_0]
 
     L_range = [2]
 
@@ -103,11 +87,10 @@ if __name__ == "__main__":
         sol = odeint(diff_eqs, y0, t)
 
         """plot output"""
-        plt.plot(t, sol[:, 0], label='EI')
-        plt.plot(t, sol[:, 1], label='EHB')
-        plt.plot(t, sol[:, 2], label='mRNA')
-        plt.plot(t, sol[:, 3], label='P')
-        plt.plot(t, sol[:, 4], label='PA')
+        plt.plot(t, sol[:, 0], label='EHB')
+        plt.plot(t, sol[:, 1], label='mRNA')
+        plt.plot(t, sol[:, 2], label='P')
+        plt.plot(t, sol[:, 3], label='PA')
         plt.ylabel('Concentration of PA (mM)')
         plt.xlabel('Time (s)')
         plt.legend(loc=1, borderaxespad=0)
