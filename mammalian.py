48d62150e8f568f5f6d0ce99133e89f7f934d528
StefaniaTrepekli <stef.trep@gmail.com>
2017-08-30 17:47:27 +0100

    Rate kinetics for mammalian cells

--- mammalian.py
+++ mammalian.py
@@ -40,33 +40,36 @@ def diff_eqs(y, t):
     '''This function contains the differential equations'''
 
     """Unpacking y"""
-    TF = y[0]  #Bound transbembrane protein by PhoCl
-    #TFb = y[1]  #Transcrption (microM/L)
-    mRNA = y[1]  #Translation (microM/L)
-    P = y[2]  #expression of the RFP protein (microM/L)
+    TF = y[0]  # Bound transbembrane protein by PhoCl
+    # TFb = y[1]  #Transcrption (microM/L)
+    mRNA = y[1]  # Translation (microM/L)
+    P = y[2]  # Expression of the RFP protein (microM/L)
 
     """Set rate constants"""  # we made these numbers up we are now looking into fixing them and adding rate equations for the k values
 
-    k2 =  452 # Rate of transcription per transcript (1/hr)
-    k3 = 12 # Rate of translation (1/hr)
-    d1=60/13 # Degradation of transcription factors (1/hr)
-    d2 = 1.68 # Degradation of mRNA (1/hr)
-    d3 = 1.86 # Degradation of translated protein (1/hr)
-    a = 400 # Rate of transport of TF from cytoplasm to the nucleus (molecules/pore/second)
-    b = 300 # Rate of transport of mRNA from nucleus to cytoplasm (1/hr)
+    k2 = 57.6  # Rate of transcription per transcript (1/hr)
+    k3 = 12  # Rate of translation (1/hr)
+    d1 = 60 / 13  # Degradation of transcription factors (1/hr)
+    d2 = 1.68  # Degradation of mRNA (1/hr)
+    d3 = 1.86  # Degradation of translated protein (1/hr)
+    a = 190.8  # Rate of transport of TF from cytoplasm to the nucleus (1/hr)
+    b = 3  # Rate of transport of mRNA from nucleus to cytoplasm (1/hr)
 
     # Rate of PhoCL being cleaved by light and transmembrane protein complex being released in the cytoplasm
-    dTF_dt = (light_cleavage*LACE)-(d1*TF)-(a*TF)
+    dTF_dt = (light_cleavage * LACE) - (d1 * TF) - (a * TF)
 
     # Rate at which TF binds to the promoter in the nucleus
-    #print(copy_number)
-    #dTFb_dt = (TF*copy_number*a)
+    # print(copy_number)
+    # dTFb_dt = (TF*copy_number*a)
 
     # Rate of transcription
-    dmRNA_dt = (k2*TF)-(d2*mRNA)-(b*mRNA)
+
+    dmRNA_dt = (k2 * TF) - (d2 * mRNA) - (b * mRNA)
 
     # Rate of translation
+    Pt = 9.26 * (10 ** -6)  # Maximum concentration of Protein the cells can produce (umol/L)
     n = (1 - (P / (Pt)));
+
     dP_dt = (n * b * k3 * mRNA) - (d3 * P)
 
     """Repack solution in same order as y"""
@@ -76,26 +79,26 @@ def diff_eqs(y, t):
 
 
 if __name__ == "__main__":
-    time_steps = 1000  # Number of timepoints to simulate
-    t = np.linspace(0, 100, time_steps)  # Set the time frame (start_time, stop_time, step) time frames are equally spaced within the two limits
+    time_steps = 100  # Number of timepoints to simulate
+    t = np.linspace(0, 5,
+                    time_steps)  # Set the time frame (start_time, stop_time, step) time frames are equally spaced within the two limits
 
     '''Set initial species concentration values'''
-    Pt = 9.26 * (10 ** -6)  # Maximum concentration of Protein the cells can produce (umol/L)
-    LACE = 500  # Initial concentra0tion of the transmembrane protein complex (units)
+    LACE = 1.54 * (10 ** -7)  # Initial concentra0tion of the transmembrane protein complex (units)
     TF_0 = 0  # Starting concentration of free TF in thej cytoplasm
-    #TFb_0= 0 # Starting conentration of the TF bound to the promoter
+    # TFb_0= 0 # Starting conentration of the TF bound to the promoter
     mRNA_0 = 0  # Starting mRNA concentration (microM/L)
     P_0 = 0  # Starting protein concentration (microM/L)
 
     '''Pack intial conditions into an array'''
     y0 = [TF_0, mRNA_0, P_0]
 
-    L_range = [0,0.005,0.01,0.015,0.02]
+    L_range = [0, 0.005, 0.01, 0.015, 0.02]
     # These are the range of light intensities who's effect was evaluated on the rate of 'k1'
 
     for L in L_range:
         print(L)
-        light_cleavage = cleavage(7.95,L,1)
+        light_cleavage = cleavage(7.95, L, 1)
         sol = odeint(diff_eqs, y0, t)
 
    # Pr_range= [50]
