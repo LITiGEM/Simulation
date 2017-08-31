a9b681a23aa73f4f0b7f23303811371ed0027dc4
StefaniaTrepekli <stef.trep@gmail.com>
2017-08-31 16:40:32 +0100

    Rate kinetics for mammalian cells

--- lightontime.py
+++ lightontime.py
@@ -8,15 +8,17 @@ k1_pulse = k1
 
 def pulsing(k1,t):
 
-        for t in range (86400):
-
-            if t>43200 and t<0:
-                k1_pulse = 0
-            else:
-                k1_pulse=k1
-                k1_pulsing_array.append(k1_pulse)
+        if t>43200 and t<0:
+            k1_pulse = 0
+        else:
+            k1_pulse=k1
+            k1_pulsing_array.append(k1_pulse)
 
         return k1_pulse
+
+
+for t in range (0,86400):
+    light_pulsing = pulsing( 458.4, t)
 
 print(k1_pulsing_array)
 
